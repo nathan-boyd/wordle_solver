22bede0a204038fa711d2644fe2edc6acee7a7df
Nathan Boyd <nathan@engineernathan.org>
2022-04-22 21:34:44 -0400

    refactor browser builder

--- browserbuilder.py
+++ browserbuilder.py
@@ -9,7 +9,7 @@ IMPLICIT_WAIT_SECONDS = 5
 
 class BrowserBuilder:
 
-    def setup_headless_webdriver(self):
+    def setup_webdriver(self, in_container):
         self.logger.info('setting chrome options')
         driver_options = webdriver.ChromeOptions()
         driver_options.add_argument('--no-sandbox')
@@ -19,16 +19,20 @@ class BrowserBuilder:
             'download.prompt_for_download': False,
         })
         self.logger.info('initializing driver')
+        if in_container:
+            self.initialize_virtual_display()
+            return webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=driver_options)
         return webdriver.Chrome(ChromeDriverManager().install(), chrome_options=driver_options)
+
+    def initialize_virtual_display(self):
+        self.logger.info('setting up virtual display')
+        self.display = Display(visible=False, size=(800, 600))
+        self.display.start()
+        self.logger.info('initialized virtual display')
 
     def __init__(self, in_container):
         self.logger = logging.getLogger("builder")
-        if in_container:
-            self.logger.info('setting up virtual display')
-            self.display = Display(visible=False, size=(800, 600))
-            self.display.start()
-            self.logger.info('initialized virtual display')
-        self.webdriver = self.setup_headless_webdriver()
+        self.webdriver = self.setup_webdriver(in_container)
         self.webdriver.implicitly_wait(IMPLICIT_WAIT_SECONDS)
         self.webdriver.set_window_size(650, 760)
         self.webdriver.set_window_position(0, 0)
--- scripts/container_start.sh
+++ scripts/container_start.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# This script runs as the start command in the docker image
-
+# this script runs as the start command in the docker image
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 APP_DIR="$(dirname "$SCRIPT_DIR")"
 
-echo "Starting Xvfb"
+echo "starting xvfb"
 
 Xvfb :1 -screen 0 1024x768x16 &> xvfb.log  &
 sleep 1
 
-echo "Starting Solver"
+echo "starting solver"
 
 export DISPLAY=:1 && python3 "$APP_DIR/main.py"
